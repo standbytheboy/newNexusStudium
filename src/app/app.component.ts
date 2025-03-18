b804ca7e3decf07c771d11eab3d4b4b99afda753
standby <moraislcs05@gmail.com>
2025-03-18 17:51:15 -0300

    resolvendo problemas de imagens e finalizando main-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { TitleComponent } from "./components/title/title.component";
+import { PopularCoursesComponent } from "./components/popular-courses/popular-courses.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, TitleComponent, PopularCoursesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/popular-courses/popular-courses.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popular-courses',
+  imports: [],
+  templateUrl: './popular-courses.component.html',
+  styleUrl: './popular-courses.component.css'
+})
+export class PopularCoursesComponent {
+
+}
