b59859b2200fc05fa7b0749f3aa513992d7f4bbb
standby <moraislcs05@gmail.com>
2025-03-21 18:32:32 -0300

    finalizando footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { TitleComponent } from "./components/title/title.component";
 import { PopularCoursesComponent } from "./components/popular-courses/popular-courses.component";
 import { StudyWithUsComponent } from "./components/study-with-us/study-with-us.component";
 import { TestimonialsComponent } from "./components/testimonials/testimonials.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, TitleComponent, PopularCoursesComponent, StudyWithUsComponent, TestimonialsComponent],
+  imports: [RouterOutlet, HeaderComponent, TitleComponent, PopularCoursesComponent, StudyWithUsComponent, TestimonialsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
