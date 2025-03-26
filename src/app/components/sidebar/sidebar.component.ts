f3cafd6a5d213a3abd9e3358d346e09d5c9e3d33
standby <moraislcs05@gmail.com>
2025-03-26 02:01:57 -0300

    finalizando sidebar component

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  @ViewChild('circle') circle!: ElementRef;
+  @ViewChild('valor') valor!: ElementRef;
+  @ViewChild('hours') hours!: ElementRef;
+  @ViewChild('total') total!: ElementRef;
 
+  valorInicial = 0;
+  valorFinal = 80;
+  
+
+  ngAfterViewInit() {
+    const progresso = setInterval(() => {
+      this.valorInicial++;
+      this.valor.nativeElement.innerHTML = `${this.total.nativeElement.textContent}<br>${this.hours.nativeElement.textContent}`;
+      this.circle.nativeElement.style.background = `conic-gradient(#E8F4FC ${this.valorInicial * 3.6}deg, #1E4C73 0deg)`;
+      
+      this.valor.nativeElement.style.fontSize = "20px"
+      if (this.valorInicial === this.valorFinal) {
+        clearInterval(progresso);
+      }
+    }, 50);
+  }
 }
