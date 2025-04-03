bd649ddb5f5788025f12b244d5c35e45ee533845
standby <moraislcs05@gmail.com>
2025-04-03 18:52:20 -0300

    iniciando componente de contribuicoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { ContributionsComponent } from "./components/contributions/contributions.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, DashboardComponent],
+  imports: [RouterOutlet, HomeComponent, DashboardComponent, ContributionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/contributions/contributions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from "../sidebar/sidebar.component";
+
+@Component({
+  selector: 'app-contributions',
+  imports: [SidebarComponent],
+  templateUrl: './contributions.component.html',
+  styleUrl: './contributions.component.css'
+})
+export class ContributionsComponent {
+
+}
