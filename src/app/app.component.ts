45886f974fe91431aa81cbc77b0068bd2f5571b4
standby <moraislcs05@gmail.com>
2025-05-04 16:28:39 -0300

    iniciando search component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from "./components/home/home.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { ContributionsComponent } from "./components/contributions/contributions.component";
 import { SettingsComponent } from "./components/settings/settings.component";
+import { SearchComponent } from './components/search/search.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, DashboardComponent, ContributionsComponent, SettingsComponent],
+  imports: [RouterOutlet, HomeComponent, DashboardComponent, ContributionsComponent, SettingsComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
