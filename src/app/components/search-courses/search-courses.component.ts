6798ca1661df5c73603a54fdfb71f62d3fba742c
standby <moraislcs05@gmail.com>
2025-05-05 01:33:29 -0300

    finalizando search articles

---
+++ src/app/components/search-courses/search-courses.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-courses',
+  imports: [],
+  templateUrl: './search-courses.component.html',
+  styleUrl: './search-courses.component.css'
+})
+export class SearchCoursesComponent {
+
+}
