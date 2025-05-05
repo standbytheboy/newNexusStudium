5b6068e93c972385b561e073fb743f62426d9543
standby <moraislcs05@gmail.com>
2025-05-05 01:42:12 -0300

    finishing search exercises

---
+++ src/app/components/search-articles/search-articles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-articles',
+  imports: [],
+  templateUrl: './search-articles.component.html',
+  styleUrl: './search-articles.component.css'
+})
+export class SearchArticlesComponent {
+
+}
---
+++ src/app/components/search-exercises/search-exercises.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-exercises',
+  imports: [],
+  templateUrl: './search-exercises.component.html',
+  styleUrl: './search-exercises.component.css'
+})
+export class SearchExercisesComponent {
+
+}
