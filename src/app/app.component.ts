d3aa7762583fe5d6236d497d1b6ba9d7f542aba4
standbytheboy <moraislcs05@gmail.com>
2025-05-12 15:37:40 -0300

    finishing video page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,12 @@ import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { ContributionsComponent } from "./components/contributions/contributions.component";
 import { SettingsComponent } from "./components/settings/settings.component";
 import { SearchExercisesComponent } from './components/search-exercises/search-exercises.component';
+import { SearchArticlesComponent } from "./components/search-articles/search-articles.component";
+import { VideoPlayerComponent } from "./components/video-player/video-player.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, DashboardComponent, ContributionsComponent, SettingsComponent, SearchExercisesComponent],
+  imports: [RouterOutlet, HomeComponent, DashboardComponent, ContributionsComponent, SettingsComponent, SearchExercisesComponent, SearchArticlesComponent, VideoPlayerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/video-player/video-player.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from "../sidebar/sidebar.component";
+
+@Component({
+  selector: 'app-video-player',
+  imports: [SidebarComponent],
+  templateUrl: './video-player.component.html',
+  styleUrl: './video-player.component.css'
+})
+export class VideoPlayerComponent {
+
+}
