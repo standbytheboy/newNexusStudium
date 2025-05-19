a923a842558471361a225a94d670c4cbfd1100dd
standbytheboy <moraislcs05@gmail.com>
2025-05-19 00:34:26 -0300

    deixando pagina semantica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./components/home/home.component";
-import { DashboardComponent } from "./components/dashboard/dashboard.component";
-import { ContributionsComponent } from "./components/contributions/contributions.component";
-import { SettingsComponent } from "./components/settings/settings.component";
-import { SearchExercisesComponent } from './components/search-exercises/search-exercises.component';
-import { SearchArticlesComponent } from "./components/search-articles/search-articles.component";
-import { VideoPlayerComponent } from "./components/video-player/video-player.component";
 import { ArticleComponent } from "./components/article/article.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, DashboardComponent, ContributionsComponent, SettingsComponent, SearchExercisesComponent, SearchArticlesComponent, VideoPlayerComponent, ArticleComponent],
+  imports: [RouterOutlet, ArticleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
